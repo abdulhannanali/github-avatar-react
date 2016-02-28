aab300a8387155a0a81f81536650ece52ef35d1d
Abdul Hannan Ali <abdulhannanali@outlook.com>
2016-02-28 19:47:06 +0500

    add error handling and improve the display in case of error

--- js/ReactComponents/Avatar.js
+++ js/ReactComponents/Avatar.js
@@ -36,14 +36,16 @@ app.AvatarImage = React.createClass({
 
 		if (this.props.userId) {
 			imgSrc = avatarEndpoint.replace("*", this.props.userId.split("-")[1])
-		}
-		else {
-			imgSrc = ""
-		}
-
-		return (
+			
+			return (
 				<img src={imgSrc} alt="avatar image" className="avatarImage" />
 			)
+		}
+		else {
+			return false
+		}
+		
+
 	}
 })
 
@@ -59,12 +61,12 @@ app.AvatarDetails = React.createClass({
 	render: function () {
 		var link = "https://github.com/*"
 
-		if (this.props.details) {
+		if (this.props.details && !_.isEmpty(this.props.details)) {
 			var details = this.props.details
 			link = link.replace("*", details.username)
 			return (
 				<div className="avatarDetails">
-					<h1 className="reposCount">{details.repos}</h1>
+					<h1 className="reposCount">Repos: {details.repos}</h1>
 					<p>Profile Link: <a className="profileLink" href={link}>{link}</a></p>
 				</div>
 				)
@@ -75,6 +77,34 @@ app.AvatarDetails = React.createClass({
 	}
 })
 
+app.AvatarError = React.createClass({
+	render: function () {
+		var message = ""
+
+		if (this.props.errorCode == 404) {
+			message = "The avatar with this username was not found in our database"
+		}
+		else if (this.props.errorCode == 500) {
+			message = "An unexpected error occured while searching for this username"
+		}
+		else if (this.props.errorCode == 403) {
+			message = "You are forbidden from the Github servers. Thank you!"
+		}
+
+		if (message) {
+			return (
+				<div className="errorAvatar">
+					{message}
+				</div>
+			)	
+		}
+		else {
+			return false
+		}
+		
+	}
+})
+
 app.Avatar = React.createClass({
 	getInitialState: function () {
 		return {
@@ -82,6 +112,7 @@ app.Avatar = React.createClass({
 			userId: "",
 			username: "",
 			reposCount: 0,
+			errorCode: 0,
 			data: {}
 		}
 	},
@@ -94,20 +125,38 @@ app.Avatar = React.createClass({
 			return;
 		}
 
-		var apiEndpoint = "https://api.github.com/legacy/user/search/*"
+		var apiEndpoint = "https://api.github.com/legacy	/user/search/*"
 		fetch(apiEndpoint.replace("*", user))
 			.then(function (response, body) {
-				return response.json()
+				if (response.status == 500 || response.status == 404) {
+					self.setState({
+						errorCode: 500
+					})
+				}
+				else if (response.status == 403) {
+					self.setState({
+						errorCode: 403
+					})
+				}
+				else if (response.status == 200) {
+					return response.json()					
+				}
 			})
 			.then(function (data) {
-				if (data.users && data.users[0]) {
+				if (data && data.users && data.users[0]) {
 					var user = data.users[0]
 					self.setState({
 						username: user.username,
 						name: user.name,
 						userId: user.id,
 						reposCount: user.repos,
+						errorCode: 0,
 						data: user
+					})
+				}
+				else {
+					self.setState({
+						errorCode: 404
 					})
 				}
 			})
@@ -115,9 +164,10 @@ app.Avatar = React.createClass({
 	render: function () {
 		return (
 				<div className="avatarForm">
-					<app.AvatarForm onFormSubmit={this.fetchUserSubmit}></app.AvatarForm>
-					<app.AvatarName name={this.state.name}></app.AvatarName>
-					<app.AvatarDetails details={this.state.data}></app.AvatarDetails>
+					<app.AvatarForm onFormSubmit={this.fetchUserSubmit} />
+					<app.AvatarError errorCode={this.state.errorCode} />
+					<app.AvatarName name={this.state.name} />
+					<app.AvatarDetails details={this.state.data} />
 					<app.AvatarImage userId={this.state.userId} />
 				</div>
 			)
